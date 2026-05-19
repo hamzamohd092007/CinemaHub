f1f7afd11c73c861c0ad2819e60d476e808377e3
Mohd Hamza <hamzamohd092007@gmail.com>
2026-05-19 17:29:15 +0530

    Update MediaCard.jsx

--- client/src/components/MediaCard.jsx
+++ client/src/components/MediaCard.jsx
@@ -18,7 +18,7 @@ const MediaCard = ({ user, media, handleWatchlist }) => {
             onClick={(e) => {
               e.preventDefault();
               e.stopPropagation();
-              handleWatchlist(String(media.id), mediaType);
+              handleWatchlist(media.id, mediaType);
             }}
             className="p-2 sm:p-3 bg-slate-700/80 hover:bg-slate-900 rounded-full transition cursor-pointer"
           >
